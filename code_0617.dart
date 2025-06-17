1a3ac2fcbd5a968b6c92a50347e5420942e834d6
wing72 <121550567+wing72@users.noreply.github.com>
2025-06-17 14:51:55 +0900

    Update code_0617.dart

--- code_0617.dart
+++ code_0617.dart
@@ -1,15 +1,11 @@
-int findWithLinearSearch(String haystack, String needle) {
-  if (needle.isEmpty) return 0;
-  
-  int m = haystack.length;
-  int n = needle.length;
-
-  for (int i = 0; i <= m - n; i++) {
-    // haystack의 i번째부터 n개의 문자를 잘라내어 needle과 통째로 비교
-    if (haystack.substring(i, i + n) == needle) {
-      return i; // 일치하면 즉시 반환
-    }
+int strStr(String s, String t) {
+  if (t.isEmpty) {
+    return 0;
   }
+  return s.indexOf(t);
+}
 
-  return -1;
+void main() {
+  print(strStr("flutterworld hello", "flutterworld"));
+  print(strStr("word", "post"));
 }
