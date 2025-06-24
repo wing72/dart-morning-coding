55c43e8d35bef090ca183c67b4ce04ba3e02a4f1
wing72 <xowkddl@gmail.com>
2025-06-24 10:35:15 +0900

    first commit

---
+++ bin/dart_mornig_codnig.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mornig_codnig/dart_mornig_codnig.dart' as dart_mornig_codnig;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_mornig_codnig.calculate()}!');
+}
---
+++ test/dart_mornig_codnig_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_mornig_codnig/dart_mornig_codnig.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
