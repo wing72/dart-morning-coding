74040a29cf03d17cb51a4b3bea807944babaf092
wing72 <xowkddl@gmail.com>
2025-06-27 10:06:44 +0900

    아침의 코딩 25.06.27

---
+++ lib/code_0626.dart
@@ -0,0 +1,29 @@
+void main(){
+//
+int result = add(2148201489201, 42819301809426019);
+int result2 = add(result, 10000);
+
+//클래스 생성한다는건 => 설계도 대로 사용할 수 있게 실제값을 만드는것
+
+Marine m1 = Marine(100, 200);
+Marine m2 = Marine(200, 200);
+Marine m3 = Marine(300, 200);
+
+Scv();
+}
+
+int add(int a, int b){
+  return a + b;
+}
+
+// 생성자 만들어서 해야 된다고 해서... 
+//클레스 및 객체
+// 클래스 => 설계도
+class Marine (
+
+Marine m1 = Marine(100, 200);
+Marine m2 = Marine(200, 200);
+Marine m3 = Marine(300, 200);
+
+Scv()
+)
